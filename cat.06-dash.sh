8533fd8d09b2a7aa923454d5968b2737127edf74
tPenguinLTG <tpenguinltg@disroot.org>
2018-07-31 17:53:36 -0400

    Read from stdin if - is supplied

---
+++ cat.06-dash.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo_stdin() {
+  while IFS= read -r line; do
+    echo "$line"
+  done
+}
+
+if [[ $# -eq 0 ]]; then
+  echo_stdin
+else
+  for file in "$@"; do
+    if [[ "$file" = "-" ]]; then
+      echo_stdin
+    elif [[ -r "$file" ]]; then
+      echo_stdin <"$file"
+    else
+      echo "${0}: ${file}: Cannot read" >&2
+    fi
+  done
+fi
